5d3ab84422d985e6f83489ddd7cbb5e31259516f
Jeffrey Muller <jeffrey.muller92@gmail.com>
2013-05-31 14:29:07 +0200

    Refactoring code, now support multi-notification bubble

--- js/jnotify.js
+++ js/jnotify.js
@@ -6,27 +6,33 @@
 
 (function($) {
 
-    var o = {};
+    var o = {}; // Options object
+    var notifId = 0;
 
     ////////////////////////
     // Ui
     ////////////////////////
 
-    setLayout = function(layout) {
-        o.htmlTemplate = $(o.htmlTemplate).attr('class', layout);
-    }
+    create = function() {
+        if (notifications.length === 0) {
+            var container = $('<div>').addClass(o.position + ' jNotify');
 
-    setContent = function(content) {
-        o.htmlTemplate = $(o.htmlTemplate).find('#content').text(content).parent();
-    }
-
-    setClosable = function(isClosable) {
-        if (isClosable === true) {
-            var closeBtn = '<button class="close">&times;</button>';
-
-            o.htmlTemplate = $(o.htmlTemplate).append(closeBtn);
-            $(o.htmlTemplate).find('button.close').click(o.onClickClose);
+            $('body').append(container);
         }
+
+        notifications.push({ options: o })
+    }
+
+    render = function() {
+        var notification = $('<div>').addClass('jNotify-notification');
+
+        if (o.isClosable === true) {
+            notification.append($('<button>').addClass('close').html('&times;'));
+        }
+
+        notification.append($('<div>').addClass('content').html(o.content));
+
+        return notification;
     }
 
     ///////////////////////
@@ -34,39 +40,45 @@
     ///////////////////////
 
     onClickClose = function() {
-        $(o.htmlTemplate).fadeOut();
+        $(this).parent().fadeOut();
     }
 
     ///////////////////////
     // Constructor
     ///////////////////////
 
+    var notifications = [];
+
     var defaults = {
         // Layout to use (must be "top left", "top right", "bottom left", "bottom right")
-        layout: 'top right',
+        position: 'top right',
 
         // Define if the notification is closable
         isClosable: true,
-        
-        // Define the HTML template of the notification bubble
-        htmlTemplate: '<div id="jnotify-box"><div id="content"></div></div>',
 
         // Content of the notification
         content: '',
 
         // Callback to call when user click on "close" button
-        onClickClose: onClickClose
+        onClickClose: onClickClose,
+
+        timeout: 5000
     };
 
     $.jNotify = function(options) {
         o = $.extend(defaults, options);
 
-        setLayout(o.layout);
-        setContent(o.content);
-        setClosable(o.isClosable);
+        create();
+        var notification = render();
 
-        $('body').append(o.htmlTemplate);
-        return this;
+        $('div.jNotify').append(notification);
+        $(notification).fadeIn();
+
+        $(notification).find('button.close').click(o.onClickClose);
+
+        setTimeout(function() {
+            $(notification).fadeOut();
+        }, o.timeout);
     }
 
 })(jQuery);
