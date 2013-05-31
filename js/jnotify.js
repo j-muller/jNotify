a88eef7d97b6d3392a68e14f5f6dc4ad66ff80c2
Jeffrey Muller <jeffrey.muller92@gmail.com>
2013-05-31 19:57:43 +0200

    Notification pool in progress

--- js/jnotify.js
+++ js/jnotify.js
@@ -7,7 +7,6 @@
 (function($) {
 
     var o = {}; // Options object
-    var notifId = 0;
 
     ////////////////////////
     // Ui
@@ -35,12 +34,53 @@
         return notification;
     }
 
+    remove = function(notification) {
+        var content = $(notification).find('div.content').html();
+
+        for (var i = 0; i < notifications.length; i++) {
+            if (notifications[i].options.content == content) {
+                notifications.splice(i, 1);
+                break;
+            }
+        }
+        $(notification).parent().hide();
+
+        if (o.pool > 0) {
+            display();
+        }
+    }
+
+    display = function() {
+        var displayed = 0;
+
+        $('div.jNotify div.jNotify-notification').each(function(idx, e) {
+            console.log('e -> ' + $(e).html() + ' | hidden ' + $(e).is(':hidden'))
+            if ($(e).is(':hidden')) {
+                // console.log('hidden... -> ' + e)
+                $(e).show();
+
+                $(e).find('button.close').click(onClickClose);
+                setTimeout(function() {
+                    remove(e);
+                }, o.timeout);
+            } else {
+                displayed++;
+            }
+
+            if (displayed == o.pool && o.pool > 0) {
+                return false;
+            }
+        });
+    }
+
     ///////////////////////
     // Callbacks
     ///////////////////////
 
     onClickClose = function() {
-        $(this).parent().fadeOut();
+        var notification = $(this).parent();
+
+        remove(notification);
     }
 
     ///////////////////////
@@ -62,7 +102,11 @@
         // Callback to call when user click on "close" button
         onClickClose: onClickClose,
 
-        timeout: 5000
+        // Time in milliseconds while bubble is displayed
+        timeout: 5000,
+
+        // Limit the number of notification displayed (0 to disable)
+        pool: 1
     };
 
     $.jNotify = function(options) {
@@ -72,13 +116,8 @@
         var notification = render();
 
         $('div.jNotify').append(notification);
-        $(notification).fadeIn();
 
-        $(notification).find('button.close').click(o.onClickClose);
-
-        setTimeout(function() {
-            $(notification).fadeOut();
-        }, o.timeout);
+        display();
     }
 
 })(jQuery);
